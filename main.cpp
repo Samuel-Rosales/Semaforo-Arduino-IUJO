c8967d017f90e18c3a3dfad22d7159f46dae72b3
Juan Sun <juansun77@gmail.com>
2026-08-01 20:48:31 -0400

    feat: definición de pines y constantes de tiempo de espera

---
+++ main.cpp
@@ -0,0 +1,14 @@
+const int VEH_NS_VERDE    = 4;
+const int VEH_NS_AMARILLO = 3;
+const int VEH_NS_ROJO     = 2;
+const int PEAT_NS_ROJO    = 5;
+const int PEAT_NS_VERDE   = 6;
+const int VEH_EO_VERDE    = 11;
+const int VEH_EO_AMARILLO = 10;
+const int VEH_EO_ROJO     = 9;
+const int PEAT_EO_VERDE   = 8;
+const int PEAT_EO_ROJO    = 7;
+
+const unsigned long T_VERDE    = 5000;
+const unsigned long T_AMARILLO = 3000;
+const unsigned long T_DESPEJE  = 1000;
