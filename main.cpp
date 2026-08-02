8dd55b82a8e36ea49a89efef1761f93c3b73e26b
EDGAR-BRI <edgardev26@gmail.com>
2026-08-01 21:12:46 -0400

    refactor: crear funciones VEH y Peat para modularizacion de leds

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@ const unsigned long T_VERDE    = 5000;
 const unsigned long T_AMARILLO = 3000;
 const unsigned long T_DESPEJE  = 1000;
 
+
+const int NS = 100, EO = 200;
+const int GRE = 300, RED = 301, YEL = 302;
+
 void setup() {
     for (int pin = 2; pin <= 11; pin++) {
     pinMode(pin, OUTPUT);
@@ -63,4 +67,41 @@ void loop() {
     digitalWrite(VEH_EO_AMARILLO, LOW);
     digitalWrite(VEH_EO_ROJO, HIGH);
     delay(T_DESPEJE);
+}
+
+
+//para el manejo de los semaforos de los carros 
+void VEH(int color, int inter){
+   int gre = LOW, red = LOW, yel = LOW;
+
+  if (color == GRE) {
+    gre = HIGH;
+  } else if (color == RED) {
+    red = HIGH;
+  } else if (color == YEL) {
+    yel = HIGH;
+  }
+
+  if(inter == NS){
+    digitalWrite(VEH_NS_ROJO, red);
+    digitalWrite(VEH_NS_VERDE, gre); 
+    digitalWrite(VEH_NS_AMARILLO, yel);
+  } else {
+    digitalWrite(VEH_EO_ROJO, red);
+    digitalWrite(VEH_EO_VERDE, gre); 
+    digitalWrite(VEH_EO_AMARILLO, yel);
+  }
+}
+
+//Simplifica el encendido/apagado invertido de los LEDs peatonales
+void Peat(bool paso, int inter){
+  bool red = !paso;
+
+  if(inter == NS){
+      digitalWrite(PEAT_NS_ROJO, red);
+      digitalWrite(PEAT_NS_VERDE, paso);
+  } else {
+      digitalWrite(PEAT_EO_ROJO, red);
+      digitalWrite(PEAT_EO_VERDE, paso);
+  }
 }
